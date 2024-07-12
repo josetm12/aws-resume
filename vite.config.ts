aa88b660a0ad3b21444eb57b80a169e4b751b8ce
jmundackaltest <josetm94mec@gmail.com>
2024-07-12 13:54:14 +1000

    Updated Vite config to copy over an error html page

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/public/error.html',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   base: './',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
