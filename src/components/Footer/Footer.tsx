7a822c4de8fa4546d770d1e4ecd1185e46a7ffbd
jmundackaltest <josetm94mec@gmail.com>
2024-07-22 17:04:58 +1000

    updated footer with count api

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react';
+import { LoaderPinwheel } from 'lucide-react';
+
+const requestOptions = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+};
+
 function Footer() {
+  const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState('NA');
   const currentYear = new Date().getFullYear();
-  const visitCountDummy = 1;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://wi2ghpsmcf.execute-api.ap-southeast-2.amazonaws.com/Prod/update',
+          requestOptions
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setCount(result.visits);
+        setLoading(false);
+      } catch (error) {
+        console.log('Fetch error', error);
+        setCount('NA');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="w-full text-center p-1 mt-10">
-      <div className="text-sm">Visitor count: {visitCountDummy}</div>
+      <div className="text-sm flex justify-center items-center">
+        Visitor count:{' '}
+        {loading ? <LoaderPinwheel className="h-4 animate-spin" /> : count}
+      </div>
       <p className="text-gray-700 text-sm">
         &copy; {currentYear} JTM. All rights reserved.
       </p>
